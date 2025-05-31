d9836dd1ae10087c7fff166e4f39bfd44bf5c8c0
bbates123 <bbates@kipsu.com>
2025-05-31 04:24:22 -0500

    Add searching thru cwd + subdirs

--- copy_from_remote.py
+++ copy_from_remote.py
@@ -1,30 +1,9 @@
 import argparse
 import duckdb
 import os
+import pathlib
 import time
 
-DUCKDB_FILE_NAME = "bc_remote.db"
-FILE_PATH = f"/Users/bradybates/Desktop/personal_projects/LOCAL_BASEBALL_COMPUTER_DATASET/data"
-DUCKDB_FILE_PATH = f"{FILE_PATH}/{DUCKDB_FILE_NAME}"
-OUTPUT_ROOT = f"{FILE_PATH}/parquet_exports"
-
-with duckdb.connect(DUCKDB_FILE_PATH) as con:
-    start = time.time()
-    skipped_count = 0
-    created_count = 0
-    target_schemas = con.execute("""
-    SELECT DISTINCT schema_name
-    FROM information_schema.schemata
-    WHERE schema_name NOT IN ('pg_catalog', 'information_schema')
-    ;
-    """).fetchdf()['schema_name'].tolist()
-
-    for schema in target_schemas:
-        print(f"SELECT DISTINCT table_name FROM information_schema.tables WHERE table_schema = '{schema}';")
-        table_names = con.execute(f"""
-        SELECT DISTINCT table_name
-        FROM information_schema.tables
-        WHERE table_schema = '{schema}'
 def export_remote_bc_files_to_parquet(args):
     bc_remote_db_path = args.bc_remote_db_path
     with duckdb.connect(bc_remote_db_path) as con:
@@ -40,20 +19,6 @@ def export_remote_bc_files_to_parquet(args):
         FROM information_schema.schemata
         WHERE schema_name NOT IN ('pg_catalog', 'information_schema')
         ;
-        """).fetchdf()['table_name'].tolist()
-        
-        for table in table_names:
-            schema_table = f"{schema}.{table}"
-            output_dir = os.path.join(OUTPUT_ROOT, schema)
-            os.makedirs(output_dir, exist_ok=True)
-            output_path = os.path.join(output_dir, schema_table) + ".parquet"
-
-            if os.path.exists(f"{output_path}"):
-                skipped_count+=1
-                print(f"{output_path} exists - skipping")
-            else:
-                added_count+=1
-                con.execute(f"""
     print(f"Finished retrieving files in {time_delta} seconds. Created: {created_count} - Skipped: {skipped_count}")
         """).fetchdf()['schema_name'].tolist()
 
@@ -62,6 +27,7 @@ def export_remote_bc_files_to_parquet(args):
             SELECT DISTINCT table_name
             FROM information_schema.tables
             WHERE table_schema = '{schema}'
+            ;
             """).fetchdf()['table_name'].tolist()
 
             for table in table_names:
@@ -85,11 +51,30 @@ def export_remote_bc_files_to_parquet(args):
         time_delta = end - start
 
 
+def find_bc_remote_db(debug: bool):
+    filename = "bc_remote.db"
+    cwd = pathlib.Path.cwd()
+    search_folders = cwd.rglob(filename)
+    for path in search_folders:
+        debug and print(f"search_path: {path}")
+        if path.exists():
+            return str(path)
+        return None
+
 parser = argparse.ArgumentParser(description="Exports all existing schemas and tables from baseball.computer's 'bc_remote.db' to Parquet files.")
 parser.add_argument("bc_remote_db_path", nargs='?', default=None, help="Path to your 'bc_remote.db' database file from the baseball.computer repo")
 parser.add_argument("output_root", help="Top level directory to save parquet files")
 parser.add_argument('-d', '--debug', default=None, action='store_true', help='Enable debug mode')
 args = parser.parse_args()
+
+if args.bc_remote_db_path is None:
+    found_path = find_bc_remote_db(args.debug)
+    if found_path is None:
+        print("Error: bc_remote.db not found in current or parent directories. Please specify in argument.")
+        exit(1)
+    else:
+        print(f"Found bc_remote.db at {found_path}")
+        args.bc_remote_db_path = found_path
 
 export_remote_bc_files_to_parquet(args)
 
